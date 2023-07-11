034b1276288cef56cd62a9ebdd8ee1df50b02fde
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-07-11 17:26:54 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,30 @@
 const router = require('express').Router()
+const places = require( '../models/places.js')
 
 // GET /places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'Salad Delish',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Vegan, Vegetarian',
-        pic: '/images/salad.jpg'
-    }, {
-        name: 'Personal Pizza',
-        city: 'Bronx',
-        state: 'NY',
-        cuisines: 'pizza, italian',
-        pic: '/images/pizza.jpg'
-        }]
-           
+router.get('/', (req, res) => { 
     res.render('places/index', {places})
   })
 
   router.get('/new', (req, res) => {
     res.render('places/new')
+  })
+  
+// POST/ places  
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
   
---
+++ models/places.js
@@ -0,0 +1,13 @@
+    module.exports = [{
+        name: 'Salad Delish',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Vegan, Vegetarian',
+        pic: '/images/salad.jpg'
+    }, {
+        name: 'Personal Pizza',
+        city: 'Bronx',
+        state: 'NY',
+        cuisines: 'pizza, italian',
+        pic: '/images/pizza.jpg'
+    }]
