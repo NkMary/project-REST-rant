a1fb9e374176ff9023332e4bd61eaee777bb426e
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-07-13 16:00:33 -0400

    added search button using bootstrap

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,7 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
-                <nav>
+                <nav class="navbar">
                     <ul>
                         <li>
                             <a href="/">Home</a>
@@ -21,8 +21,15 @@ function Def (html) {
                             <a href="/places/new">Add Place</a>
                         </li>
                     </ul>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                         <button class="btn btn-sm btn-outline-secondary" type="submit">Search</button>
+                    </form>
                 </nav>
                 {html.children}
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
             </body>
         </html>
     )
