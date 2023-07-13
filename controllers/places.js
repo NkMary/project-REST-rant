bf7e52ccd60371373c27184bf2d5b76d212c5baa
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-07-13 17:39:23 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
     res.render('places/new')
   })
 
+//SHOW
   router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -40,25 +41,64 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
+  
+  
+//EDIT
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('/places/edit')
+  }
+})
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${req.params.id}`)
+  }
+})
+
+
+  
 //DELETE
-  router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      places.splice(id, 1)
-      res.redirect('/places')
-    }
-  })
-  
-  
-/* router.get('/edit', (req, res)=> {
-    res.render('places/edit')
-}) */
-  
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 module.exports = router
